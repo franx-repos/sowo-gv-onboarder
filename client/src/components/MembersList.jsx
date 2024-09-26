5bcc0fd319f963d7e9dcd680a6a502e901db0208
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-09-26 23:10:21 +0200

    added features

--- client/src/components/MembersList.jsx
+++ client/src/components/MembersList.jsx
@@ -1,35 +1,66 @@
-const MembersList = ({ users }) => {
-  const tableStyle = {
+import { useState } from "react";
+
+const MembersList = ({ members }) => {
+  const style = {
+    btn: "h-fit mr-5 bg-pink-700 hover:bg-pink-800 py-2 px-4 text-white hover:text-white rounded justify-center",
     table:
-      "table-auto border-collapse border border-slate-400 dark:border-slate-500 bg-white dark:bg-slate-800 text-lg shadow-sm",
-    thead: "bg-slate-50 dark:bg-slate-700",
-    th: "w-1/2 border border-slate-300 dark:border-slate-600 font-semibold p-3 text-slate-900 dark:text-slate-200 text-left",
-    td: "border border-slate-300 dark:border-slate-700 p-3 text-slate-500 dark:text-slate-400 text-left",
+      "table-auto border-collapse border border-neutral-500 bg-neutral-800 text-lg",
+    th: "w-1/2 border border-neutral-600 font-semibold p-3 text-neutral-200 text-left bg-pink-700 hover:bg-pink-800 cursor-pointer",
+    td: "border border-neutral-700 p-3 text-neutral-300 text-left",
+    p: "ml-3 text-white text-lg p-2",
+  };
+
+  const [sortedMembers, setSortedMembers] = useState(members);
+
+  const sortMembers = (key) => {
+    const sortedArray = [...members].sort((a, b) => {
+      if (typeof a[key] === "number" && typeof b[key] === "number") {
+        return a[key] - b[key];
+      }
+      return a[key].localeCompare(b[key]);
+    });
+    setSortedMembers(sortedArray);
   };
 
   return (
-    <div className="mt-10">
-      <table className={tableStyle.table}>
-        <thead className={tableStyle.thead}>
+    <div className="flex mt-10">
+      <button className={style.btn} onClick={() => setSortedMembers(members)}>
+        Reset
+      </button>
+
+      <table className={style.table}>
+        <thead className={style.thead}>
           <tr>
-            <th className={tableStyle.th}>Sowo-Nr.</th>
-            <th className={tableStyle.th}>Name</th>
-            <th className={tableStyle.th}>Haus</th>
-            <th className={tableStyle.th}>Ankunftszeit</th>
+            <th className={style.th} onClick={() => sortMembers("sowo_id")}>
+              Sowo-Nr.
+            </th>
+            <th className={style.th} onClick={() => sortMembers("name")}>
+              Name
+            </th>
+            <th className={style.th} onClick={() => sortMembers("house")}>
+              Haus
+            </th>
+            <th
+              className={style.th}
+              onClick={() => sortMembers("timeOfArrival")}
+            >
+              Ankunftszeit
+            </th>
           </tr>
         </thead>
         <tbody>
-          {users &&
-            users?.map((user) => (
-              <tr key={user._id}>
-                <td className={tableStyle.td}>{user.sowo_id}</td>
-                <td className={tableStyle.td}>{user.name}</td>
-                <td className={tableStyle.td}>{user.house}</td>
-                <td className={tableStyle.td}>{user.timeOfArrival}</td>
+          {sortedMembers &&
+            sortedMembers.map((sMember) => (
+              <tr key={sMember._id}>
+                <td className={style.td}>{sMember.sowo_id}</td>
+                <td className={style.td}>{sMember.name}</td>
+                <td className={style.td}>{sMember.house}</td>
+                <td className={style.td}>{sMember.timeOfArrival}</td>
               </tr>
             ))}
         </tbody>
       </table>
+      <p className={style.p}>&#8592; zum Sortieren klicken</p>
     </div>
   );
 };
--- client/src/components/NavigationTop.jsx
+++ client/src/components/NavigationTop.jsx
@@ -5,19 +5,17 @@ const NavigationTop = ({ setCurrentLocation }) => {
 
   return (
     <>
-      <nav className="bg-white dark:bg-gray-900 w-full border-b border-gray-200 dark:border-gray-600">
-        <div className=" flex flex-wrap items-center justify-between p-4">
+      <nav className="bg-pink-700 w-full">
+        <div className="flex flex-wrap items-center justify-between p-2">
           <a href="#" className="flex items-center">
             <img
-              src="https://sowo-eg.org/wp-content/uploads/sites/70/2017/09/sowo_logo_prov-300x272.jpg"
-              className="h-8"
+              src="./src/assets/Sowo-Logo.png"
+              className="h-12 mr-3"
               alt="Sowo Logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Sowo
-            </span>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
           </a>
-          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <div className="flex md:order-2 space-x-3 md:space-x-0">
             {/* <button
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -27,7 +25,7 @@ const NavigationTop = ({ setCurrentLocation }) => {
             <button
               data-collapse-toggle="navbar-sticky"
               type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-neutral-500 rounded-lg md:hidden hover:bg-neutral-100 focus:outline-none focus:ring-2 focus:ring-neutral-200 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:ring-neutral-600"
               aria-controls="navbar-sticky"
               aria-expanded="false"
             >
@@ -53,30 +51,38 @@ const NavigationTop = ({ setCurrentLocation }) => {
             className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-sticky"
           >
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-neutral-100 rounded-lg bg-pink-700 md:space-x-8 md:flex-row md:mt-0 md:border-0 dark:border-neutral-700">
               <li>
                 <a
                   onClick={() => handleNavigation("scanner")}
                   href="#"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 text-2xl"
+                  className="block p-2 text-white text-2xl bg-pink-700 hover:bg-neutral-800 rounded-md"
                   aria-current="page"
                 >
-                  scanner
+                  <img
+                    src="./src/assets/scan-icon.png"
+                    alt="scanner"
+                    className="h-12"
+                  />
                 </a>
               </li>
               <li>
                 <a
                   onClick={() => handleNavigation("members")}
                   href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-2xl"
+                  className="block p-2 text-white text-2xl bg-pink-700 hover:bg-neutral-800 rounded-md"
                 >
-                  members
+                  <img
+                    src="./src/assets/list-icon.png"
+                    alt="members list"
+                    className="h-12"
+                  />
                 </a>
               </li>
               {/* <li>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 px-3 text-neutral-900 rounded hover:bg-neutral-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-neutral-700"
                 >
                   Services
                 </a>
@@ -84,7 +90,7 @@ const NavigationTop = ({ setCurrentLocation }) => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 px-3 text-neutral-900 rounded hover:bg-neutral-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-neutral-700"
                 >
                   Contact
                 </a>
