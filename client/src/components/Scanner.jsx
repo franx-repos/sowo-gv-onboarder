242f3a53bca9919916e71242ac2401bb5fd4df39
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-10-05 10:26:41 +0200

    cleaned up home

--- client/src/components/Scanner.jsx
+++ client/src/components/Scanner.jsx
@@ -1,7 +1,13 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Html5QrcodeScanner } from "html5-qrcode";
 
-const Scanner = ({ setScanSuccess, setScanResult }) => {
+const Scanner = ({ scanResult, setScanResult }) => {
+  const [scanSuccess, setScanSuccess] = useState(false);
+
+  const style = {
+    p: "mb-3 text-white text-xl p-2 rounded-md bg-neutral-800",
+  };
+
   useEffect(() => {
     const scanner = new Html5QrcodeScanner("qr-reader", {
       qrbox: {
@@ -27,9 +33,23 @@ const Scanner = ({ setScanSuccess, setScanResult }) => {
   }, []);
 
   return (
-    <>
-      <div id="qr-reader"></div>
-    </>
+    <div className="flex my-auto">
+      <div
+        className={`p-5 rounded-lg ${
+          scanSuccess === true
+            ? "border-4 border-teal-700 pulse"
+            : "border-4 border-pink-700"
+        }`}
+      >
+        <div id="qr-reader"></div>
+      </div>
+      <div className="flex flex-col ml-5">
+        <p className={style.p}>Sowo-Id: {scanResult?.sowo_id}</p>
+        <p className={style.p}>Vorname: {scanResult?.firstName}</p>
+        <p className={style.p}>Nachname: {scanResult?.lastName}</p>
+        <p className={style.p}>Haus: {scanResult?.house}</p>
+      </div>
+    </div>
   );
 };
 export default Scanner;
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ function Sidebar({ membersCount }) {
     p: "mb-3 text-white text-xl p-2 rounded-md bg-neutral-800",
     btn: "bg-pink-700 hover:bg-pink-800 py-2 px-4 text-white hover:text-white rounded-md inline-flex mt-auto justify-center",
     input:
-      "w-20 text-xl rounded-e-md mb-3 p-2 bg-neutral-800  placeholder-neutral-400 text-white focus:border-pink-700",
+      "w-20 text-xl rounded-e-md mb-3 p-2 bg-neutral-800  placeholder-neutral-400 text-white focus:outline-none focus:ring focus:ring-pink-700 focus:rounded-md",
   };
 
   return (
@@ -31,7 +31,7 @@ function Sidebar({ membersCount }) {
       <p className={style.p}>anwesend: {membersCount}</p>
       <p
         className={`mb-3 text-white text-xl p-2 rounded-md ${
-          hasQuorum ? "bg-green-500" : "bg-red-500"
+          hasQuorum ? "bg-teal-500" : "bg-red-500"
         }`}
       >
         Quorum: {hasQuorum ? "ja" : "nein"}
