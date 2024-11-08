0f5c53a31ec8d2760a0a8d455074cf43de51da8e
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-11-08 13:05:32 +0100

    moved Downloadbutton

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -5,7 +5,7 @@ import NavigationTop from "./NavigationTop";
 import Sidebar from "./Sidebar";
 import ManualInput from "./ManualInput";
 import useDateTime from "../hooks/useDateTime";
-import CsvDownloadButton from "../hooks/CsvDownloadButton";
+import CsvDownloadButton from "./CsvDownloadButton";
 import useMembers from "../hooks/useMembers";
 /*https://www.npmjs.com/package/react-csv */
 
