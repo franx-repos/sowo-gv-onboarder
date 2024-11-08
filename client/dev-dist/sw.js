17ae68b90d9f53f853ed1cdfa71fa041aa191518
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-11-08 13:04:32 +0100

    created hooks

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ftneg6p8gp"
+    "revision": "0.vg18u4vbp8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
